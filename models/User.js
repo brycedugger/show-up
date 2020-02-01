12ce46ac696f8937b4f56cad3183f1744e833d11
msheffield <msheffield@adaptivebiotech.com>
2020-02-01 12:10:16 -0800

    user has first, last and user names

--- models/User.js
+++ models/User.js
@@ -1,18 +1,33 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-  saved: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Event"
-    }
-  ],
-  created: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Event"
-    }
-  ]
+    firstName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    userName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    saved: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ],
+    created: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ]
 
     //fill in required parameters used in /utils/APi.js
     //and corresponding API routes in the routes foler
