97e7e60f77e318562ec962b5597e3f95fe06d98b
msheffield <msheffield@adaptivebiotech.com>
2020-02-01 10:26:31 -0800

    added username and fixed Date in comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
 const CommentSchema = mongoose.Schema({
-  body: {
-    type: String,
-    required: true
-  },
-  date: { 
-      type: String,
-      trim: true,
-      required: true 
+    username: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    body: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
     },
 });
 
