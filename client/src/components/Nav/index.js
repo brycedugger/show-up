b9069abc9dbd8f127f473c5c0cd941194a8cc8f6
Annie <annie.yiyunc@gmail.com>
2020-02-02 17:49:52 -0800

    addded date picke to Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import DatePicker from "react-date-picker";
 
 function Nav() {
     return (
@@ -8,11 +8,11 @@ function Nav() {
                 Show Up
             </a>
             <ul className='navbar-nav'>
-            <form class="form-inline">
-                <div class="md-form my-0">
-                    <input type="search" class="form-control ds-input" id="search-input" placeholder="Search..." />
-                </div>
-            </form>
+                <form class="form-inline">
+                    <div class="md-form my-0">
+                        <input type="search" class="form-control ds-input" id="search-input" placeholder="Search..." />
+                    </div>
+                </form>
                 <li className='nav-item'>
                     <a className="navbar-brand" href="/event">
                         Event
@@ -23,11 +23,14 @@ function Nav() {
                         New Event
                     </a>
                 </li>
-                <li className='nav-item'>
+                <DatePicker />
+
+                <li className='nav-item ml-auto'>
                     <a className="navbar-brand" href="/profile">
                         Profile
                     </a>
                 </li>
+
             </ul>
         </nav>
     );
