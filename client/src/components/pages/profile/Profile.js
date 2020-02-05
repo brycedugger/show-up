f6d138fffb097d22e4b4526883028a5b4a4bd388
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-05 00:27:40 -0800

    starter code for profile page; css styling added temporarily for visualization of containers and page set up

--- client/src/components/pages/profile/Profile.js
+++ client/src/components/pages/profile/Profile.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
-
+import { Col } from "react-bootstrap";
+import "./style.css";
 
 class Profile extends Component {
+  render() {
+    return (
+      <div className="profileContainer">
+        <Col>
+          <ProfileSideBar />
+        </Col>
+        <Col>
+          <ProfileMainContent />
+        </Col>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            <div className="container">
-                <div className="container mb-5 mt-5">
-                    <p>Hello from Profile Page</p>
-                </div>
-            </div>
-        );
-    }
+class ProfileSideBar extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="sideBarStyle">
+          <p>Hello from Side Bar Page</p>
+        </div>
+      </div>
+    );
+  }
+}
 
+class ProfileMainContent extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="mainStyle">
+          <p>Hello from Main Content Page</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
