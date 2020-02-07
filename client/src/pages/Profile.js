c83fc1270ee26085f2ccf27c442fd4dd50bab8e6
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-06 22:34:53 -0800

    created new profile branch to remedy conflicts with master. Adding updates and changes from other profile branch to this one for commit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
-
+import ProfileSideBarInfo from "../pages/profile-components/ProfileSideBarInfo";
+import EditProfileBtn from "../pages/profile-components/EditProfileBtn";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 class Profile extends Component {
+  render() {
+    return (
+      <div className="profileContainer">
+        <Row>
+          <Col xs={6} md={4}>
+            <ProfileSideBar />
+          </Col>
+          <Col>
+            <ProfileBookmarkContent />
+          </Col>
+          <Col md={{ span: 6, offset: 4 }}>
+            <ProfileCreatedEventContent />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            <div className="container">
-                <div className="container mb-5 mt-5">
-                    <p>Hello from Profile Page</p>
-                </div>
-            </div>
-        );
-    }
+class ProfileSideBar extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="sideBarStyle">
+          <ProfileSideBarInfo />
+          <EditProfileBtn />
+        </div>
+      </div>
+    );
+  }
+}
 
+class ProfileBookmarkContent extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="mainStyle">
+          <h2>Your Bookmarked Events</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ProfileCreatedEventContent extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="createdEventsStyle">
+          <h2>Your Created Events</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
