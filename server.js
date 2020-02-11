5549aa735d0fa3fa61240ff30b2c7ad3acee41f7
Bryce Dugger <bryce.dugger@outlook.com>
2020-02-10 17:32:38 -0800

    no errors on server after auth boilerplate additions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const db = require('./models');
 // for tokens
 const jwt = require("jsonwebtoken"); 
 // passport
