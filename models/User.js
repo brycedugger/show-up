efb9c8617794d35ae0015bdbbf47a79a185cfa13
msheffield <msheffield@adaptivebiotech.com>
2020-02-12 19:58:27 -0800

    updated model and seed with email and password

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
+    email: {
         type: String,
         trim: true,
         required: true,
