af9b1b0d2bac402b310bd209a70fded2c0b0550e
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-12 20:09:53 -0800

    added a modal to the Edit Profile button

--- client/src/pages/profile-components/EditProfileBtn.js
+++ client/src/pages/profile-components/EditProfileBtn.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal } from 'react-bootstrap';
 import "./style.css"
 
-function EditProfileBtn(props) {
-  return (
-    <div>
-      <button className="editProfile">
+function EditProfileBtn() {
+  const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
         Edit Profile
-      </button>
-    </div>
-  );
-}
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Profile</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+  // render (<EditProfileBtn />);
 
-export default EditProfileBtn;
+  export default EditProfileBtn;
