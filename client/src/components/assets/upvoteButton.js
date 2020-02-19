88baa98b77e5a1c20a2ac0e6731647dabdacf25e
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-19 01:46:56 -0800

    finished adding in upvot button function. Need to test to see whether button counts

--- client/src/components/assets/upvoteButton.js
+++ client/src/components/assets/upvoteButton.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Button from "react-bootstrap/Button";
 
 function PostButton(props) {
     let style = {
-        width:24,
+        width: 24,
         height: 24,
         color: 'blue'
     };
     return (
-        <Button style={style} onClick={() => props.handleClick()}>{props.label}
-       </Button>
+        <Button style={style}onClick={() => props.handleClick()}>{props.label} 
+        </Button>
     );
 }
 
@@ -18,39 +18,71 @@ function Text(props) {
         border: "1px solid blue",
         width: props.width
     };
-    return(
+    return (
         <div style={style}>{props.text}</div>
     );
 }
 
 function Post(props) {
-    let style = {  
+    let style = {
         display: "flex"
     };
     return (
         <div style={style}>
-            <PostButton label = '+' handleClick = {props.increaseCount}/>
-            <Text text = {props.count} width = "20" />
-            <PostButton label = '-' handleClick = {props.decreaseCount}/>
+            <PostButton label='+'handleClick={props.increaseCount}/>
+            <Text text={props.count}width="20"/>
+            <PostButton label='-'handleClick={props.decreaseCount}/>
         </div>
     );
 }
 
 function PostUpvoteButton(props) {
-    return(
-        <div>
-            {
-                props.postUpvoteButton.map((item) => (
-                    <Post key = {index}
-                        score = {item.count}
-                        increaseCount = {() => props.updateCount(index, 1)}
-                        decreaseCount = {() => props.updateCount(index, -1)}
-                    />
-                ))
-            }
-        </div>
+    return (
+        <div> {
+            props.postUpvoteButton.map((item) => (
+                <Post key={index}
+                    score={item.count}
+                    increaseCount={() => props.updateCount(index, 1)}
+                    decreaseCount={() => props.updateCount(index, -1)}
+                />
+            ))
+        } </div>
     )
 }
 
+class UpvoteButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "",
+            items: []
+        }
+    }
 
-// export default UpvoteButton;
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    updateCount(index, val) {
+        let itemsCopy = this.state.items.slice();
+        itemsCopy[index].score += val;
+        itemsCopy.sort((a,b) => {
+            return b.score - a.score;
+        })
+        this.setState({
+            items: itemsCopy
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <input value={this.state.value} onChange={this.handleChange.bind(this)}/>
+                <PostUpvoteButton postUpvoteButton = {this.state.items}
+                updateCount = {this.updateCount.bind(this)}/>
+            </div>
+        );
+    }
+}
+
+export default UpvoteButton;
