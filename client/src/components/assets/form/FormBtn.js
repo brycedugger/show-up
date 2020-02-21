eb03de4cc632bc35481d2127d978221585281428
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-20 20:09:27 -0800

    adjusted a few css styling on the form button

--- client/src/components/assets/form/FormBtn.js
+++ client/src/components/assets/form/FormBtn.js
@@ -7,7 +7,7 @@ import "./formbuttonStyle.css";
 
 export function FormBtn(props) {
     return (
-        <button {...props} style={{ float: "right", marginBottom: 10 }} className="formButton">
+        <button {...props} style={{ marginBottom: 10 }} className="formButton">
             {props.children}
 
         </button>
