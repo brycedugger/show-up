db79a8f0b94b3f2bc6b0ac4f34c43e636580c1ad
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-20 17:23:01 -0800

    fixed formatting in file

--- client/src/components/assets/bookmarkButton.js
+++ client/src/components/assets/bookmarkButton.js
@@ -1,58 +1,49 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Button from "react-bootstrap/Button";
 import API from "../utils/API";
 import UserAPI from "../utils/userAPI";
 
 class BookmarkButton extends Component {
 
-  state = {
-    userId: ""
-  };
-   componentDidMount() {
-     const token = localStorage.getItem("token");
-     this.handleGetUser(token);
-   }
-
-   handleGetUser = token => {
-    UserAPI.getUser(token)
-    .then(res => {
-      this.setState({
-        userId: res.data._id
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  };
-
-  addBookmarkedEventToUsesrDB = data => {
-    UserAPI.bookmarkEvent({
-      userId: this.state.userId,
-      _id: data._id
-    })
-    .then(res => {
-      if(res.status === 200) {
-        this.setRedirect();
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-
-  render() {
-    if (this.state.redirect === true) {
-      return <Redirect to="/profile" />
+    state = {
+        userId: ""
+    };
+    componentDidMount() {
+        const token = localStorage.getItem("token");
+        this.handleGetUser(token);
     }
 
-    return (
-      <Button variant="outline-success">Bookmark</Button>
-    );
-  }
+    handleGetUser = token => {
+        UserAPI.getUser(token).then(res => {
+            this.setState({userId: res.data._id});
+        }).catch(err => {
+            console.log(err);
+        })
+    };
+
+    addBookmarkedEventToUsesrDB = data => {
+        UserAPI.bookmarkEvent({userId: this.state.userId, _id: data._id}).then(res => {
+            if (res.status === 200) {
+                this.setRedirect();
+            }
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    render() {
+        if (this.state.redirect === true) {
+            return <Redirect to="/profile"/>
+        }
+
+        return (
+            <Button variant="outline-success">Bookmark</Button>
+        );
+    }
 }
 
 export default BookmarkButton;
 
 
 // get user id from profile page
-// and the event id that will be a prop 
+// and the event id that will be a prop
