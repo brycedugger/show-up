6f8471fc05c3a54e5eb0c3fc0698f201d3ca40e0
hoffs <stephanie.nhoffman7@gmail.com>
2020-02-20 17:21:12 -0800

    building out route and functionality for the bookmark button. Have it getting a handle on the userId

--- client/src/components/assets/bookmarkButton.js
+++ client/src/components/assets/bookmarkButton.js
@@ -4,7 +4,47 @@ import API from "../utils/API";
 import UserAPI from "../utils/userAPI";
 
 class BookmarkButton extends Component {
+
+  state = {
+    userId: ""
+  };
+   componentDidMount() {
+     const token = localStorage.getItem("token");
+     this.handleGetUser(token);
+   }
+
+   handleGetUser = token => {
+    UserAPI.getUser(token)
+    .then(res => {
+      this.setState({
+        userId: res.data._id
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
+
+  addBookmarkedEventToUsesrDB = data => {
+    UserAPI.bookmarkEvent({
+      userId: this.state.userId,
+      _id: data._id
+    })
+    .then(res => {
+      if(res.status === 200) {
+        this.setRedirect();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
+    if (this.state.redirect === true) {
+      return <Redirect to="/profile" />
+    }
+
     return (
       <Button variant="outline-success">Bookmark</Button>
     );
@@ -12,3 +52,7 @@ class BookmarkButton extends Component {
 }
 
 export default BookmarkButton;
+
+
+// get user id from profile page
+// and the event id that will be a prop 
