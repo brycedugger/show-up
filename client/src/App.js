010afe4ff8f299fe0f1e49cd15be99143e434ca3
msheffield <msheffield@adaptivebiotech.com>
2020-02-21 23:52:46 -0800

    will have to revert if this doesn't work

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Event from "./pages/Event"
 import NewEventForm from "./pages/event-forms/NewEventForm";
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
 
-      <Router history={hashHistory}>
+      <Router>
         <div>
           <MainNav
             onClick={this.handleFormSubmit}
