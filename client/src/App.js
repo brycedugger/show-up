46f9e9f8f53048e97f77c3f4bae65a33a52fb0b0
msheffield <msheffield@adaptivebiotech.com>
2020-02-21 23:15:06 -0800

    moved router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,19 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Event from "./pages/Event"
+import NewEventForm from "./pages/event-forms/NewEventForm";
+import UpdateEventForm from "./pages/event-forms/UpdateEventForm";
+import Profile from "./pages/Profile";
+import LoginForm from "./pages/login-signup/LoginForm";
+import SignupForm from "./pages/login-signup/SignupForm";
+
 import MainNav from "./components/Nav";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 class App extends Component {
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   if () {
-  //     API.getSingleEvent()
-  //       .then(res => this.loadBooks())
-  //       .catch(err => console.log(err));
-  //   }
-  // };
 
   state = {
     isLoggedIn: false
@@ -36,7 +35,17 @@ class App extends Component {
         <MainNav 
           onClick={this.handleFormSubmit} 
           isLoggedIn={this.state.isLoggedIn}/>
-        <Main />
+        <Router>
+            <Switch>
+              <Route path='/login' component={LoginForm} />
+              <Route path='/signup' component={SignupForm} />
+              <Route path='/newevent' component={NewEventForm} />
+              <Route path='/event/:id' component={Event} />
+              <Route path='/profile' component={Profile} />
+              <Route path='/update/:id' component={UpdateEventForm} />
+              <Route path='/' component={Home} />
+            </Switch>
+          </Router>
         <Footer />
       </React.Fragment>
     );
